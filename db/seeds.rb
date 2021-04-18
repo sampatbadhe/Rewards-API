44ca6dcbcd6a8f6fe3634bd7d37357db1ee9d526
sampatbadhe <sampat.badhe@kiprosh.com>
2021-04-18 21:31:49 +0530

    #7 - db seed to add categories, category_reasons, claim_grant_statuses, badges

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+%i[Approved Rejected Withdrawn Pending].each do |status|
+  ClaimGrantStatus.find_or_create_by!(granted_status: status)
+end
+
+%i[Gold Silver Bronze].each do |badge|
+  Badge.find_or_create_by!(name: badge)
+end
+
+
+{
+  'KFC' => { 'Meeting' => 'Bronze', 'Organize Event' => 'Silver' },
+  'COE' => { 'Meeting' => 'Bronze', 'Organize Event' => 'Silver' },
+  'Hiring' => { 'Interview' => 'Bronze', 'Code Review' => 'Bronze', 'Prepared question set' => 'Bronze' },
+  'Referral' => { 'Gifted a Career' => 'Gold' },
+  'Others' => { 'New Initiative Implemented' => 'Bronze', 'Improved Existing Process' => 'Bronze', 'Other' => 'Bronze' }
+}.each do |category_name, category_reasons|
+  category = Category.find_or_create_by!(name: category_name)
+  category_reasons.each do |reason, badge|
+    category_reason = CategoryReason.find_or_create_by(category_id: category.id, reason: reason)
+    CategoryReasonBadge.find_or_create_by(category_id: category.id, category_reason_id: category_reason.id, badge_id: Badge.find_or_create_by!(name: badge).id)
+
+  end
+end
