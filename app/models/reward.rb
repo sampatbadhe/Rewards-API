432eee007c1ffcf7b02ee16f87cd51e21ea173b7
saunmanoj888 <manoj@kiprosh.com>
2021-04-18 18:03:19 +0530

    Add association of category to reward

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,4 +2,7 @@ class Reward < ApplicationRecord
   validates_presence_of :activity_date
 
   belongs_to :user
+  belongs_to :category
+  belongs_to :category_reason
+  belongs_to :claim_grant_status
 end
