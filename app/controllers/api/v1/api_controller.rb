6b318695953c5cd83727c97658fb6d80ca2a0b2e
saunmanoj888 <62114687+saunmanoj888@users.noreply.github.com>
2021-04-18 09:07:58 +0530

    Move api specific code from ApplicationController to ApiController (#15)

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,12 +2,15 @@ module Api
   module V1
     class ApiController < ApplicationController
       include ParamReader
+      include ActionController::Helpers
+      include TokenValidatable
 
       resource_description do
         api_version 'v2'
       end
 
       skip_before_action :validate_token, only: [:error]
+      skip_before_action :verify_authenticity_token
 
       # --------------
       # Rescue Errors
@@ -20,6 +23,9 @@ module Api
       # -------------
       # Helpers
       # -------------
+
+      helper_method :current_user
+      before_action :current_user
 
       def render_error(message, code: :unprocessable_entity)
         render_api_error ApiErrors::BaseError.new(message, status: code)
@@ -37,6 +43,27 @@ module Api
 
       def render_api_error(error)
         render json: error.as_json, status: error.status
+      end
+
+      # Read token data from request Authorization header
+      # Parse the user_id
+      # Assign the current_user
+      def current_user
+        @current_user ||= read_from_token
+      end
+
+      def read_from_token
+        return nil unless token.valid? && token_data.present?
+
+        user_id = token.read('user_id')
+        user = User.find_by(id: user_id)
+        return nil if user.nil?
+
+        user
+      end
+
+      def token_data
+        token.data && token.data[0]['user_id']
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include ActionController::Helpers
-  include TokenValidatable
-  skip_before_action :verify_authenticity_token
-
-  helper_method :current_user
-  before_action :current_user
-
-  private
-
-  # Read token data from request Authorization header
-  # Parse the user_id
-  # Assign the current_user
-  def current_user
-    @current_user ||= read_from_token
-  end
-
-  def read_from_token
-    return nil unless token.valid? && token_data.present?
-
-    user_id = token.read('user_id')
-    user = User.find_by(id: user_id)
-    return nil if user.nil?
-
-    user
-  end
-
-  def token_data
-    token.data && token.data[0]['user_id']
-  end
 end
