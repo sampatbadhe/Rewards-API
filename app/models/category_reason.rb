5693365317258cd57bad660bbb4925cdddf3ac69
sampatbadhe <sampat.badhe@kiprosh.com>
2021-04-24 14:47:38 +0530

    #36 - Declare enum as a Hash not Array

--- app/models/category_reason.rb
+++ app/models/category_reason.rb
@@ -1,5 +1,16 @@
 class CategoryReason < ApplicationRecord
-  enum status: %i[gold silver bronze]
+  enum badge: {
+    gold: 0,
+    silver: 1,
+    bronze: 2
+  }, _default: 'bronze'
+
+  validates_presence_of :reason
+  validates_presence_of :badge
 
   belongs_to :category
+
+  def name
+    reason
+  end
 end
