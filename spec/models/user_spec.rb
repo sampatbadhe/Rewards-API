b4087751550abff12fa6488051d19387acaf5ca5
saunmanoj888 <manoj@kiprosh.com>
2021-04-24 17:19:04 +0530

    feature-32 - User model spec , Change should to is_expected.to

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe User, type: :model do
   let(:user) { create(:user, first_name: 'John', last_name: 'Doe') }
   let(:new_user) { build(:user) }
 
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:google_uid) }
-  it { should validate_presence_of(:first_name) }
-  it { should validate_presence_of(:last_name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:google_uid) }
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
 
-  it { should have_many(:rewards).dependent(:destroy) }
+  it { is_expected.to have_many(:rewards).dependent(:destroy) }
 
   describe 'Instance Methods' do
     describe '#full_name' do
