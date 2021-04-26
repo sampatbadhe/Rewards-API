3377ca3a77ec6e2d525309996505fa8dc07ec570
saunmanoj888 <manoj@kiprosh.com>
2021-04-26 23:49:00 +0530

    feature-21 Change my_view default response

--- app/serializers/my_view_serializer.rb
+++ app/serializers/my_view_serializer.rb
@@ -2,7 +2,7 @@ class MyViewSerializer < ActiveModel::Serializer
   attributes :badges_by_category, :badges_tally
 
   def badges_by_category
-    badge_counts = {}
+    badge_counts = { 'KFC' => 0, 'COE' => 0, 'Hiring' => 0, 'Referral' => 0, 'Others' => 0 }
     object.rewards.approved.group_by(&:category).each do |category, rewards|
       badge_counts[category.name] = rewards.count
     end
@@ -10,7 +10,8 @@ class MyViewSerializer < ActiveModel::Serializer
   end
 
   def badges_tally
-    badges = object.rewards.approved.includes(:category_reason).map { |r| r.badge }
-    badges.tally
+    default_badges_count = { 'gold' => 0, 'silver' => 0, 'bronze' => 0 }
+    badges = object.rewards.approved.includes(:category_reason).map(&:badge)
+    badges.tally.merge!(default_badges_count) { |_key, old_value, new_value| [old_value, new_value].max }
   end
 end
