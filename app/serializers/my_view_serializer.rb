bef6df6e2163d397736e8f6db36cf08b98d5b52b
saunmanoj888 <62114687+saunmanoj888@users.noreply.github.com>
2021-04-27 13:07:31 +0530

    feature-21 Change my_view default response (#47)
    * feature-21 Change my_view default response
    
    * feature-21 Refactor code
    
    * feature-21 Remove extra space
    
    * feature-21 Code Review

--- app/serializers/my_view_serializer.rb
+++ app/serializers/my_view_serializer.rb
@@ -2,7 +2,7 @@ class MyViewSerializer < ActiveModel::Serializer
   attributes :badges_by_category, :badges_tally
 
   def badges_by_category
-    badge_counts = {}
+    badge_counts = Hash[Category.pluck(:name).collect { |cg_name| [cg_name, 0] }]
     object.rewards.approved.group_by(&:category).each do |category, rewards|
       badge_counts[category.name] = rewards.count
     end
@@ -10,7 +10,8 @@ class MyViewSerializer < ActiveModel::Serializer
   end
 
   def badges_tally
-    badges = object.rewards.approved.includes(:category_reason).map { |r| r.badge }
-    badges.tally
+    default_badges_count = { 'gold' => 0, 'silver' => 0, 'bronze' => 0 }
+    badges = object.rewards.approved.includes(:category_reason).map(&:badge)
+    default_badges_count.merge(badges.tally)
   end
 end
